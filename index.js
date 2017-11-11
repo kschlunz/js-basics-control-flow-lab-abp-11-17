4fe94a18fa7f2dc724b7aae8386059ab0b4a01f7
Kate Schlunz <schlunzk@gmail.com>
2017-11-11 18:03:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your code in this file!
+// Write your code in this file!function scuberGreetingForFeet(feet){
+function scuberGreetingForFeet(feet){
+if (feet <= 400){
+  return "This one is on me!"
+}
+
+else if (feet >= 2000 && feet <= 2499){
+  return "I will glad take your thirty bucks."
+}
+
+else if (feet >= 2500){
+  return "No can do."
+}
+}
+
+
+
+switch (switchOnCharmFromTip){
+      case 'generous':
+          console.log('Thank you so much.');
+          break;
+
+      case 'not as generous':
+          console.log('Thank you.');
+          break;
+
+      default:
+        console.log('Bye.');
+  }
