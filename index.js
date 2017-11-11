8055d2051b950a3ccedf2ad36d4718c9e761aef6
Kate Schlunz <schlunzk@gmail.com>
2017-11-11 21:20:22 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+function switchOnCharmFromTip(phrase){
+switch(phrase) {
+      case 'generous':
+          return('Thank you so much.');
+          break;
+
+      case 'not as generous':
+          return('Thank you.');
+          break;
+
+      default:
+        return('Bye.');
+  }
+
