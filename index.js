6013ed39116d22088c219d41ef0dbe5229928e42
Kate Schlunz <schlunzk@gmail.com>
2017-11-11 21:09:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,18 @@ else if (feet >= 2500){
   return "No can do."
 }
 }
+
+
+function switchOnCharmFromTip(phrase){
+switch(phrase) {
+      case 'generous':
+          console.log('Thank you so much.');
+          break;
+
+      case 'not as generous':
+          console.log('Thank you.');
+          break;
+
+      default:
+        console.log('Bye.');
+  }
